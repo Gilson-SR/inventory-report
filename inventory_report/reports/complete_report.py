1ff9a0e40892f8bc6b5e191f8de2fc54a840376e
Gilson-SR <gilsonsrodrigues87@gmail.com>
2023-05-09 14:42:17 -0300

    feat: Requisito 3

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,26 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(products_list):
+        simple_report = SimpleReport.generate(products_list)
+
+        companies = {}
+
+        for product in products_list:
+            company = product["nome_da_empresa"]
+            if company in companies:
+                companies[company] += 1
+            else:
+                companies[company] = 1
+
+        label_products_company = "Produtos estocados por empresa:\n"
+
+        for company in companies:
+            label_products_company += f"- {company}: {companies[company]}\n"
+
+        return (
+            f"{simple_report}\n"
+            f"{label_products_company}"
+        )
