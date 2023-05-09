13c06a5d8b44d00b328e31f3fbbc03d13119ced0
Gilson-SR <gilsonsrodrigues87@gmail.com>
2023-05-09 16:47:18 -0300

    feat: Requisito 6

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xml.etree.ElementTree as ET
 
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
@@ -7,23 +8,47 @@ from inventory_report.reports.complete_report import CompleteReport
 
 class Inventory:
     @staticmethod
+    def _load_csv(path):
+        with open(path, newline='') as file:
+            reader = csv.DictReader(file)
+            return [row for row in reader]
+
+    @staticmethod
+    def _load_json(path):
+        with open(path) as file:
+            return json.load(file)
+
+    @staticmethod
+    def _load_xml(path):
+        tree = ET.parse(path)
+        roots = tree.getroot()
+        stock = []
+        for root in roots:
+            item = {}
+            for source in root:
+                item[source.tag] = source.text
+            stock.append(item)
+        return stock
+
+    @staticmethod
     def import_data(path, report_type):
-        extension = path.split(".")[-1]
-        if extension == "csv":
-            with open(path, newline='') as file:
-                reader = csv.DictReader(file)
-                stock = [row for row in reader]
-        elif extension == "json":
-            with open(path) as file:
-                stock = json.load(file)
-        else:
-            raise ValueError("Extensão de arquivo inválida")
+        loaders = {
+            "csv": Inventory._load_csv,
+            "json": Inventory._load_json,
+            "xml": Inventory._load_xml,
+        }
 
         if report_type == "simples":
-            report = SimpleReport.generate(stock)
+            generator = SimpleReport.generate
         elif report_type == "completo":
-            report = CompleteReport.generate(stock)
+            generator = CompleteReport.generate
         else:
             raise ValueError("Tipo de relatório inválido")
 
+        extension = path.split(".")[-1]
+        if extension not in loaders:
+            raise ValueError("Extensão de arquivo inválida")
+
+        stock = loaders[extension](path)
+        report = generator(stock)
         return report
